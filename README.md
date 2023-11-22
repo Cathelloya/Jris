0ba56e33085fb007fe29eeb0d25f07e533612293
peron <peron_ap@icloud.com>
2023-11-22 15:40:15 +0000

    feat: basic web server

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Jris
 
 Jris is a WIP QQ chatbot written in Python.
+
+## Usage
+
+1. Set up `go-cqhttp` in `cqhttp` folder.
+2. Configure `.env` according to `.env.example`.
+3. Run `python3 src/main.py` to start the bot.
--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 from aiohttp import web
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 
 async def handle(request):
-    return web.Response(text="Hello, world!.")
+    return web.Response(text="Hello, world!")
 
 
 app = web.Application()
 app.add_routes([web.get("/", handle)])
 
 if __name__ == "__main__":
-    web.run_app(app)
+    web.run_app(app, port=os.getenv("LISTEN_PORT"))
