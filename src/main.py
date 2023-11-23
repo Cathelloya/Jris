1298d2da9c05e1b4b78d4ea8b88260bfb2b41241
peron <peron_ap@icloud.com>
2023-11-23 16:22:57 +0000

    feat: reply to group message

--- src/main.py
+++ src/main.py
@@ -8,10 +8,22 @@ import os
 async def handle_message(data: dict[str]):
     if data['message_type'] == 'private':
         msg = reply_dict.reply_to(data.get('raw_message')) or "我听不懂。"
-        user_id = data.get('user_id')
+        user_id = data['user_id']
 
         await client.send_message({
             "user_id": user_id,
+            "message": msg
+        })
+    elif data['message_type'] == 'group':
+        user_msg: str = data['raw_message']
+        if not user_msg[:5].lower() == 'jris ':
+            return
+
+        msg = reply_dict.reply_to(user_msg[5:]) or "我听不懂。"
+        group_id = data['group_id']
+
+        await client.send_message({
+            "group_id": group_id,
             "message": msg
         })
 
