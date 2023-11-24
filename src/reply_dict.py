1f73239026053da861b1dc412a8fd1d310a59939
peron <peron_ap@icloud.com>
2023-11-24 13:17:49 +0000

    feat: advanced reply pattern matching

--- src/reply_dict.py
+++ src/reply_dict.py
@@ -1,14 +1,30 @@
 import random
+import re
+from typing import Union
 
-reply_dict: dict[str, str | list[str]] = {
-    "在吗": "在的",
-    "摸摸头": ["好捏！", "不去追求现实中实际存在的人，而来找机器人寻求安慰，难道不是贯彻了荒诞主义吗？"],
+ReplyDictValue = Union[str, list[str]]
+ReplyDict = dict[str, ReplyDictValue]
+
+reply_dict: ReplyDict = {
+    "^不要(.*)": "放心，我不会{}",
+    "^你?在吗.*": "在的",
+    "^摸摸头$": ["好捏！", "不去追求现实中实际存在的人，而来找机器人寻求安慰，难道不是贯彻了荒诞主义吗？"],
+    "(.*)在哪里\\??": "{0}在{0}附近"
 }
 
 
 def reply_to(msg: str) -> str | None:
     msg = msg.strip()
-    reply = reply_dict.get(msg)
+
+    reply: ReplyDictValue | None = None
+    matches: list[str] = []
+
+    for k, v in reply_dict.items():
+        new_matches = re.match(k.format(*matches), msg)
+        if new_matches:
+            reply = v
+            matches.extend(new_matches.groups())
+            break
 
     if reply is None:
         return None
@@ -16,4 +32,16 @@ def reply_to(msg: str) -> str | None:
     if isinstance(reply, list):
         return random.choice(reply)
 
-    return reply
+    try:
+        return reply.format(*matches)
+    except IndexError:
+        return None
+
+
+if __name__ == '__main__':
+    print(reply_to("在吗?"))
+    print(reply_to("你在吗?"))
+    print(reply_to("他在吗?"))
+    print(reply_to("不要回复"))
+    print(reply_to("摸摸头"))
+    print(reply_to("你在哪里"))
