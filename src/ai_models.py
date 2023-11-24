0742ab5bda5712e19e9398198ecf71ada5e36d82
peron <peron_ap@icloud.com>
2023-11-24 00:49:54 +0000

    fix: unable to make request

--- src/ai_models.py
+++ src/ai_models.py
@@ -1,11 +1,12 @@
 from aiohttp import ClientSession
+import asyncio
 
 
 async def instruct_llm(instruction: str) -> str | None:
     url = "https://ai.ihack.uk/api/run/@cf/mistral/mistral-7b-instruct-v0.1"
     prompt = '<s>[INST] ' + instruction.strip() + ' [/INST]'
     async with ClientSession() as session:
-        async with session.post(url, data={"prompt": prompt}) as response:
+        async with session.get(url, params={"prompt": prompt}) as response:
             json = await response.json()
             if response.ok:
                 return json['result']['response']
